065c6c8f3f9252f71dacb8bc2e84d1d3bbb0f1c3
sankarvenkatesan <sankareee1930@gmail.com>
2021-09-17 23:04:12 +0530

    Task done

---
+++ src/org/javaproject/Sample123.java
@@ -0,0 +1,10 @@
+package org.javaproject;
+
+public class Sample123 {
+	public static void main(String[] args) {
+		System.out.println("and");
+		System.out.println("dnd");
+		System.out.println("pnd");
+	}
+
+}
